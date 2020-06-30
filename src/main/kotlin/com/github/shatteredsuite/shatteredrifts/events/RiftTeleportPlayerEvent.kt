1e3ff686fb982702d88a0d136032c0a898132e99
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2020-06-30 01:07:11 -0600

    :hammer: Clarify teleport event type.

--- src/main/kotlin/com/github/shatteredsuite/shatteredrifts/events/RiftTeleportPlayerEvent.kt
+++ src/main/kotlin/com/github/shatteredsuite/shatteredrifts/events/RiftTeleportPlayerEvent.kt
@@ -4,5 +4,4 @@ import com.github.shatteredsuite.shatteredrifts.data.RiftLocation
 import org.bukkit.entity.Player
 import org.bukkit.event.player.PlayerTeleportEvent
 
-class RiftTeleportPlayerEvent(player: Player, val rift: RiftLocation) : PlayerTeleportEvent(player, player.location, rift.destination) {
-}
+class RiftTeleportPlayerEvent(player: Player, val rift: RiftLocation) : PlayerTeleportEvent(player, player.location, rift.destination, TeleportCause.PLUGIN)
